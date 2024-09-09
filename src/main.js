4ae8c689b9e9294e11c5aa37f74103f3c77aede8
Chiahsuan-TW <belinda05021992@gmail.com>
2024-09-09 23:46:25 +0800

    fix: 調整樣式檔

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './assets/main.css'
-import 'leaflet/dist/leaflet.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './assets/styles/index.css'
+import 'leaflet/dist/leaflet.css'
+
 
 const app = createApp(App)
 
