fcd9be8f92120d5864595cdd5bb0a5a3d4fd89bc
Chiahsuan-TW <belinda05021992@gmail.com>
2024-09-11 23:31:25 +0800

    feat: 新增錯誤頁面

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import { useMiddleware } from './middleware'
 
 const Login = () => import('@/views/Login/IndexView.vue')
 const Home = () => import('@/views/Home/IndexView.vue')
+const NotFound = () => import('@/views/NotFound/IndexView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,11 @@ const router = createRouter({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
